7b15ee94e7a47e52c8163479dce82c35a63f6b83
rsanchez <sanchez.castillo.raul@gmail.com>
2019-12-22 18:01:50 +0100

    [ST-006] order line domain model tests, improving readability

--- src/test/kotlin/com/rsanchezc/taxes/domains/OrderLineTest.kt
+++ src/test/kotlin/com/rsanchezc/taxes/domains/OrderLineTest.kt
@@ -1,76 +1,38 @@
 package com.rsanchezc.taxes.domains
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.assertAll
 
 class OrderLineTest {
+    private val musicCD = Product( id = 1, name = "music CD", price = 14.99, category = Category.OTHERS)
+    private var orderLine = OrderLine(product = musicCD, taxes = 10.0F)
 
-    @Test
-    fun `single product subtotal` () {
-        val product = Product(
-            id = 1,
-            name = "music CD",
-            price = 14.99,
-            category = Category.OTHERS
-        )
-        val orderLine = OrderLine(product = product, taxes = 10.0F)
-
-        assertEquals(16.49, orderLine.calculateSubTotal())
+    @BeforeEach
+    fun init() {
+        orderLine = OrderLine(product = musicCD, taxes = 10.0F)
     }
 
     @Test
-    fun `single product subtotal taxes` () {
-        val product = Product(
-            id = 1,
-            name = "music CD",
-            price = 14.99,
-            category = Category.OTHERS
-        )
-        val orderLine = OrderLine(product = product, taxes = 10.0F)
-
-        assertEquals(1.50, orderLine.calculateSubTotalTaxes())
-    }
+    fun `single product subtotal` () = assertEquals(16.49, orderLine.calculateSubTotal())
 
     @Test
-    fun `print single product` () {
-        val product = Product(
-            id = 1,
-            name = "music CD",
-            price = 14.99,
-            category = Category.OTHERS
-        )
-        val orderLine = OrderLine(product = product, taxes = 10.0F)
+    fun `single product subtotal taxes` () = assertEquals(1.50, orderLine.calculateSubTotalTaxes())
 
-        assertEquals("1 music CD: 16.49", "$orderLine")
-    }
+    @Test
+    fun `print single product` () = assertEquals("1 music CD: 16.49", "$orderLine")
 
     @Test
     fun `print 2 products` () {
-        val product = Product(
-            id = 1,
-            name = "music CD",
-            price = 14.99,
-            category = Category.OTHERS
-        )
-        val orderLine = OrderLine(product = product, amount = 2, taxes = 10.0F)
+        val orderLine = OrderLine(product = musicCD, amount = 2, taxes = 10.0F)
 
         assertEquals("2 music CD: 32.98", "$orderLine")
     }
 
     @Test
     fun `set negative amount` () {
-        val orderLine = OrderLine(
-            product = Product(
-                id = 1,
-                name = "music CD",
-                price = 14.99,
-                category = Category.OTHERS
-            ),
-            taxes = 10.0F
-        )
-
         assertThrows<IllegalArgumentException>{
             orderLine.setAmountTo(-1)
         }
@@ -78,16 +40,6 @@ class OrderLineTest {
 
     @Test
     fun `set amount to 3`() {
-        val orderLine = OrderLine(
-            product = Product(
-                id = 1,
-                name = "music CD",
-                price = 14.99,
-                category = Category.OTHERS
-            ),
-            taxes = 10.0F
-        )
-
         orderLine.setAmountTo(3)
 
         assertAll("order line",
