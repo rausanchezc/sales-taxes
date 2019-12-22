5deb296ad0d530b77ed7de09fc425061d0ea56cc
rsanchez <sanchez.castillo.raul@gmail.com>
2019-12-22 18:11:10 +0100

    [ST-008] receipt service tests, improving readability

--- src/test/kotlin/com/rsanchezc/taxes/services/ReceiptServiceTest.kt
+++ src/test/kotlin/com/rsanchezc/taxes/services/ReceiptServiceTest.kt
@@ -10,6 +10,21 @@ import org.junit.jupiter.api.Test
 
 class ReceiptServiceTest {
     private val receiptService: ReceiptService = ReceiptService(TaxesService())
+    private val productsInput1 = listOf(
+        Product(id = 1, name = "book", price = 12.49, category = Category.BOOKS),
+        Product(id = 2, name = "music CD", price = 14.99, category = Category.OTHERS),
+        Product(id = 3, name = "chocolate bar", price = 0.85, category = Category.FOOD)
+    )
+    private val productsInput2 = listOf(
+        Product(id = 4, name = "imported box of chocolates", price = 10.00, category = Category.FOOD, isImported = true),
+        Product(id = 5, name = "imported bottle of perfume", price = 47.50, category = Category.OTHERS, isImported = true)
+    )
+    private val productsInput3 = listOf(
+        Product( id = 6, name = "imported bottle of perfume", price = 27.99, category = Category.OTHERS, isImported = true),
+        Product( id = 7, name = "bottle of perfume", price = 18.99, category = Category.OTHERS),
+        Product( id = 8, name = "packet of headache pills", price = 9.75, category = Category.MEDICINES),
+        Product( id = 9, name = "box of imported chocolates", price = 11.25, category = Category.FOOD, isImported = true)
+    )
     private var receipt = Receipt()
 
     @BeforeEach
@@ -19,9 +34,9 @@ class ReceiptServiceTest {
 
     @Test
     fun `INPUT 1`() {
-        val book = Product(id = 1, name = "book", price = 12.49, category = Category.BOOKS)
-        val musicCD = Product(id = 2, name = "music CD", price = 14.99, category = Category.OTHERS)
-        val chocolateBar = Product(id = 3, name = "chocolate bar", price = 0.85, category = Category.FOOD)
+        val book = productsInput1[0]
+        val musicCD = productsInput1[1]
+        val chocolateBar = productsInput1[2]
 
         receiptService.addProduct(receipt, book)
         receiptService.addProduct(receipt, musicCD)
@@ -35,20 +50,8 @@ class ReceiptServiceTest {
 
     @Test
     fun `INPUT 2` () {
-        val importedBoxOfChocolates = Product(
-            id = 1,
-            name = "imported box of chocolates",
-            price = 10.00,
-            category = Category.FOOD,
-            isImported = true
-        )
-        val importedBottleOfPerfume = Product(
-            id = 2,
-            name = "imported bottle of perfume",
-            price = 47.50,
-            category = Category.OTHERS,
-            isImported = true
-        )
+        val importedBoxOfChocolates = productsInput2[0]
+        val importedBottleOfPerfume = productsInput2[1]
 
         receiptService.addProduct(receipt, importedBoxOfChocolates)
         receiptService.addProduct(receipt, importedBottleOfPerfume)
@@ -61,32 +64,10 @@ class ReceiptServiceTest {
 
     @Test
     fun `INPUT 3` () {
-        val importedBottleOfPerfume = Product(
-            id = 1,
-            name = "imported bottle of perfume",
-            price = 27.99,
-            category = Category.OTHERS,
-            isImported = true
-        )
-        val bottleOfPerfume = Product(
-            id = 2,
-            name = "bottle of perfume",
-            price = 18.99,
-            category = Category.OTHERS
-        )
-        val pills = Product(
-            id = 3,
-            name = "packet of headache pills",
-            price = 9.75,
-            category = Category.MEDICINES
-        )
-        val importedBoxOfChocolates = Product(
-            id = 4,
-            name = "box of imported chocolates",
-            price = 11.25,
-            category = Category.FOOD,
-            isImported = true
-        )
+        val importedBottleOfPerfume = productsInput3[0]
+        val bottleOfPerfume = productsInput3[1]
+        val pills = productsInput3[2]
+        val importedBoxOfChocolates = productsInput3[3]
 
         receiptService.addProduct(receipt, importedBottleOfPerfume)
         receiptService.addProduct(receipt, bottleOfPerfume)
